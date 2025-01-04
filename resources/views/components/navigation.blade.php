1bbafe816512e84744575406f4c4d9d55caa5718
Victor <vk561244@gmail.com>
2025-01-04 11:28:39 -0600

    x

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -2,7 +2,7 @@
     <ul>
         <li><a href="{{ route('inicio') }}">Salir</a></li>
         <li><a href="{{ route('welcome') }}">Tienda</a></li>
-        <li><a href="{{ route('productos') }}">Mis Productos</a></li>
+        <li><a href="{{ route('productos') }}">Mis productos</a></li>
         <li><a href="{{ route('pedidosV') }}">Pedidos</a></li>
         <li><a href="{{ route('historial') }}">Historial de transacciones</a></li>
     </ul>
